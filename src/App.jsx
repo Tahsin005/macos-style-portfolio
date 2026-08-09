c7e7b160d36c2196876acaa4cbad6c986b97c67f
tahsin005 <tahsin.ferdous3546@gmail.com>
2026-08-10 00:57:59 +0600

    feat: add Contact window to the application stack

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import gsap from "gsap"
 import { Draggable } from "gsap/Draggable"
 
 import { Navbar, Welcome, Dock } from "#components"
-import { Finder, Image, Resume, Safari, Terminal, Text } from "#windows"
+import { Contact, Finder, Image, Resume, Safari, Terminal, Text } from "#windows"
 
 gsap.registerPlugin(Draggable)
 
@@ -18,6 +18,7 @@ const App = () => {
       <Finder />
       <Text />
       <Image />
+      <Contact />
     </main>
   )
 }
