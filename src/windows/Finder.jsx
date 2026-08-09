c6650c1ba0aa9d1ba387be1bcff74b3eaebd05e8
tahsin005 <tahsin.ferdous3546@gmail.com>
2026-08-10 00:51:33 +0600

    feat: add Image and Text window components with support for .git file types

--- src/windows/Finder.jsx
+++ src/windows/Finder.jsx
@@ -13,7 +13,7 @@ const Finder = () => {
     const openItem = (item) => {
         if (item.fileType === "pdf") return openWindow("resume");
         if (item.kind === "folder") return setActiveLocation(item);
-        if (["fig", "url"].includes(item.fileType) && item.href)
+        if (["fig", "url", "git"].includes(item.fileType) && item.href)
             return window.open(item.href, "_blank");
 
         openWindow(`${item.fileType}${item.kind}`, item);
